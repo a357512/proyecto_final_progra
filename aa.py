0648558dc2d36611c706ac1e4cc4cfa7146d0e24
a357512 <118298261+a357512@users.noreply.github.com>
2022-11-17 00:46:00 -0600

    Update aa.py

--- aa.py
+++ aa.py
@@ -49,15 +49,21 @@ if imc < 18.5:
   st.image('https://www.colchonescefiro.es/blog/wp-content/uploads/2015/10/personas-imc-bajo.png')
   st.write('Las personas con esta condición pueden tener un sistema inmunológico débil, así como una pobre condición física, haciéndolos propensos a padecer infecciones. Otros riesgos que puede causar el bajo peso son: Problemas de fertilidad, especialmente en mujeres. También problemas durante el embarazo.')
   st.write('_Top Doctors España_')
+  st.subheader('Recomendación')
+  st.write('Agregue calorías saludables. Se pueden aumentar las calorías agregando coberturas de nueces o semillas, queso y guarniciones saludables como con almendras, semillas de girasol, frutas o tostadas de trigo integral.')
 if (imc>18.5 and imc<24.9):
   st.write('IMC saludable')
   st.image('https://www.colchonescefiro.es/blog/wp-content/uploads/2015/10/personas-imc-medio.png')
   st.write('Su índice de masa corporal indica que está saludable, mantenga sus hábitos saludables para conservar su IMC saludable')
+  st.subheader('Recomendación')
+  st.write('Al día realiza mínimo 30 minutos de actividad deportiva, haz estiramientos que favorecen a tu circulación.)
 if (imc>25 and imc<29.9):
   st.write('IMC indica sobrepeso')
   st.image('https://www.colchonescefiro.es/blog/wp-content/uploads/2015/10/personas-imc-alto.png')
   st.write('El sobrepeso no es una enfermedad, pero es una condición que predispone al desarrollo de enfermedades tales como diabetes, presión elevada (hipertensión), elevación de grasas en sangre (dislipidemias), infartos, embolias, algunos tipos de cáncer y favorece la muerte prematura')
   st.write('_SEMAR, s.f._')
+  st.subheader('Recomendación')
+  st.write('Disminuye en tu dieta la carne grasa, la mantequilla, el aceite de palma; las comidas procesadas como pizzas congeladas, galletas, snacks de paquete, entre otros.')         
  
  
  
@@ -67,11 +73,16 @@ if (imc>30 and imc<39.9):
   st.image('https://www.colchonescefiro.es/blog/wp-content/uploads/2015/10/personas-imc-alto.png')
   st.write('La obesidad es una enfermedad crónica, producida por el consumo excesivo de grasas, azúcares y carbohidratos, lo que provoca la acumulación de grasa en nuestro cuerpo. Otra causa es la poca o nula actividad física en la vida diaria.')
   st.write('_Presidencia de la República EPN, 2013_')
+  st.subheader('Recomendación')
+  st.write('Establece horarios al comer. Procura comer tus alimentos al horno, salteados, y no fritos. Reducir el consumo de azúcar. Reducir el consumo de sal.')
 if imc >40 and imc<99.99:
   st.write('IMC indica obesidad mórbida')
   st.image('https://www.colchonescefiro.es/blog/wp-content/uploads/2015/10/personas-imc-alto.png')
   st.write('Las personas con obesidad mórbida frecuentemente presentan hipertensión arterial, diabetes mellitus, cardiopatía coronaria, insuficiencia respiratoria y dislipidemia; además de lo anterior, pueden padecer limitaciones físicas para realizar actividades debido a problemas osteoarticulares derivados de la obesidad extrema.')
   st.write('_Secretaria de Salud, 2015_')
+  st.subheader('Recomendación')
+  st.write('Consulta a tu nutriólogo')
+  st.write('Correo nutriólog FCQ UACH: ')
 if imc >100.01:
  st.write('IMC indica obesidad mórbida')
  st.image('https://www.colchonescefiro.es/blog/wp-content/uploads/2015/10/personas-imc-alto.png')
